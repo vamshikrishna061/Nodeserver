8bc7557630c0b60b3645b3576f48530b710eeb15
vamshikrishna061 <kandagatlavamshikrishna11>
2023-03-20 15:52:52 +0530

    TASK 7

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const http = require('http');
 
-const server = http.createServer((request, response)=>{
-   response.statusCode = 200;
-   response.setHeader('content-Type', 'text/plain');
-   response.end('Hello vamshiKrishna')
+// const server = http.createServer((request, response)=>{
+//    response.statusCode = 200;
+//    response.setHeader('content-Type', 'text/plain');
+//    response.end('Hello vamshiKrishna')
+// });
+// server.listen(4000);
+
+//const server = http.createServer((request, response)=>{
+    //console.log(request.url, request.method, request.headers);
+    // response.setHeader('content-Type', 'text/html');
+    // response.write('<html>');
+    // response.write('<head><title> My First Page</title><head>');
+    // response.write('<body><h1> Hello from my Node.js server!</h1><nodu>');
+    // response.write('<html>');
+    // response.end();
+    
+//});
+const server = http.createServer((request,respose)=>{
+    const  url  = request.url;
+  if (url === '/home') {
+    respose.setHeader( 'Content-Type', 'text/plain' );
+    respose.write('Welcome home');
+    respose.end();
+  } else if (url === '/about') {
+    respose.setHeader('Content-Type', 'text/plain');
+    respose.write('Welcome to About Us page');
+    respose.end();
+  }
+  else if (url === '/node') {
+    respose.setHeader('Content-Type', 'text/plain');
+    respose.write('Welcome to my Node Js project');
+    respose.end();
+  } 
 });
-server.listen(4000);
+server.listen(3000);
+ 
